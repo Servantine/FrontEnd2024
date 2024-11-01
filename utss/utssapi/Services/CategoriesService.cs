82e7cf29bce59b1a3a23929551412f13f1c82cef
Servantine <mocachino47@gmail.com>
2024-11-01 21:06:10 +0700

    UTS

---
+++ utss/utssapi/Services/CategoriesService.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using utssapi.Models;
+
+namespace utssapi.Services
+{
+    public class CategoriesService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoriesService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Categories>> GetCategoriesAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Categories>>("api/v1/Categories");
+        }
+
+        public async Task<Categories> GetCategoryByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Categories>($"api/v1/Categories/{id}");
+        }
+
+        public async Task UpdateCategoryAsync(Categories categories)
+        {
+            await _httpClient.PutAsJsonAsync($"api/v1/Categories/{categories.categoryId}", categories);
+        }
+
+        public async Task DeleteCategoryAsync(int id)
+        {
+            await _httpClient.DeleteAsync($"api/v1/Categories/{id}");
+        }
+
+
+        public async Task<Categories> CreateCategoryAsync(Categories newCategory)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/v1/Categories", newCategory);
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                return await response.Content.ReadFromJsonAsync<Categories>();
+            }
+            else
+            {
+
+                throw new HttpRequestException($"Gagal menambahkan kategori. Status code: {response.StatusCode}");
+            }
+        }
+    }
+}
---
+++ utss/utssapi/Services/CoursesService.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using utssapi.Models;
+
+namespace utssapi.Services
+{
+    public class CoursesService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CoursesService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Course>> GetCoursesAsync() 
+        {
+            return await _httpClient.GetFromJsonAsync<List<Course>>("api/Courses");
+        }
+
+        public async Task<Course> GetCourseByIdAsync(int id)  
+        {
+            return await _httpClient.GetFromJsonAsync<Course>($"api/Courses/{id}");
+        }
+
+        public async Task UpdateCourseAsync(Course course) 
+        {
+            await _httpClient.PutAsJsonAsync($"api/Courses/{course.courseId}", course);
+        }
+
+        public async Task DeleteCourseAsync(int id)
+        {
+            await _httpClient.DeleteAsync($"api/Courses/{id}");
+        }
+
+        public async Task<Course> CreateCourseAsync(Course newCourse)  
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/v1/Courses", newCourse);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<Course>();
+            }
+            else
+            {
+                throw new HttpRequestException($"Failed to add course. Status code: {response.StatusCode}");
+            }
+        }
+    }
+}
